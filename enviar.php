8e14d5757e96931a9d8b8589e4286328cdfa6b5d
joaoHass <smartpocketbr@gmail.com>
2021-11-10 15:53:29 -0300

    refactored to the new enviar.php

--- enviar.php
+++ enviar.php
@@ -1,9 +1,10 @@
 <?php
 
 // Passando os dados obtidos pelo formulário para as variáveis abaixo e sanitizando
-$nomeremetente    = htmlspecialchars($_POST['name-form'], ENT_QUOTES);
 $emailremetente   = 'no-reply@blancmarketingdigital.com.br';
+$nome             = htmlspecialchars($_POST['name-form'], ENT_QUOTES);
 $telefone      	  = htmlspecialchars($_POST['cellphone-form'], ENT_QUOTES);
+$email            = htmlspecialchars($_POST['email-form'], ENT_QUOTES);
 $gasto            = htmlspecialchars($_POST['gasto-form'], ENT_QUOTES);
 $url              = htmlspecialchars($_POST['url-form'], ENT_QUOTES);
 $emailatt_cliente = 'contato.sunwatts@gmail.com.br';
@@ -13,7 +14,7 @@ $emailatt_leads   = 'leads@blancmarketingdigital.com.br';
 $corpoHTML = '<strong>Formulário de Contato</strong>
 
 <p><b>Nome:</b>                     '.$nomeremetente.'  </p>
-<p><b>E-Mail:</b>                   '.$emailremetente.' </p>
+<p><b>E-Mail:</b>                   '.$email.'          </p>
 <p><b>Telefone:</b>                 '.$telefone.'       </p>
 <p><b>Gasto com energia (R$):</b>   '.$gasto.'          </p>
 <hr>';
@@ -37,8 +38,8 @@ $header_leads    = $headers . "Return-Path: $emailatt_leads \r\n";
 $envio_leads     = mail($emailatt_leads,"[Lead] Sunwatts", $corpoHTML_leads, $header_leads); 
 
 
-// if($envio_cliente && $envio_leads) {
-//   echo "<script>location.href='sucesso.html'</script>"; // Página que será redirecionada
-// }
+if($envio_cliente && $envio_leads) {
+  echo "<script>location.href='sucesso.html'</script>"; // Página que será redirecionada
+}
 
 ?>
